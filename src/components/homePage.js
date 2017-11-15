7230102546b655bef6be9b988ce4eee9fcac6eb4
Chris Palic <chrispalic@chrispalic.com>
2017-11-14 20:53:34 -0500

    Change for the branch

--- src/components/homePage.js
+++ src/components/homePage.js
@@ -10,6 +10,7 @@ var Home = React.createClass({
             <div className="jumbotron">
                 <h1>Expedite Client Site Application</h1>
                 <p>Soon I can start putting the forms, and other cool things here. Then it will be a real app!</p>
+                <p>This is just a change so I can commit to a branch</p>
                 <Link to="about" className="btn btn-primary btn-lg">Learn more</Link>
             </div>
         );
